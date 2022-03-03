97611b3dfa7cd82ebacb9fdf259bc0ba97e1bb95
Júlio César <74342193+jlcesarr@users.noreply.github.com>
2022-03-03 01:39:38 -0300

    feat: menu for mobile devices and improvements

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+const mobileMenu = {
+    toggleButton: document.querySelector('#bx'),
+    menu: document.querySelector('.dropdown-menu'),
+    menuItems: document.querySelectorAll('.dropdown-menu-list > .dropdown-item')
+}
+
+// TOGGLE MOBILE MENU
+
+let isMobileMenuOpen = false;
+mobileMenu.toggleButton.addEventListener('click', () => {
+    isMobileMenuOpen = !isMobileMenuOpen;
+
+    mobileMenu.menu.classList.toggle('is-active', isMobileMenuOpen);
+})
+
+
+// TOGGLE MENU ITEM
+
+const removeAllActiveLinks = () => mobileMenu.menuItems.forEach((item) => item.classList.remove('is-active'));
+
+
+mobileMenu.menu.addEventListener('click', (event) => {
+    const { target: element } = event;
+
+    if (element.classList.contains('dropdown-link')) {
+        removeAllActiveLinks();
+        element.parentElement.classList.add('is-active')
+    }
+})
